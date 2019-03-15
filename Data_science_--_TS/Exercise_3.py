b16f8c730dccf5c6f0643bc9962c0b38dc42a8a1
TJHeeringa <heeringa@protonmail.ch>
2019-03-15 12:56:14 +0100

    Finished Exercise 5

--- Data_science_--_TS/Exercise_3.py
+++ Data_science_--_TS/Exercise_3.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-ACTIVITY = {
+ACTIVITY_DICT = {
     1: "WALKING",
     2: "WALKING_UPSTAIRS",
     3: "WALKING_DOWNSTAIRS",
@@ -10,32 +10,32 @@ ACTIVITY = {
     5: "STANDING",
     6: "LAYING",
 }
+if __name__ == '__main__':
+    train_data = pd.read_csv("UCI+HAR+Dataset/train/X_train.txt", header=None, sep='\s+')
+    train_label = pd.read_csv("UCI+HAR+Dataset/train/Y_train.txt", header=None, sep='\s+')
 
-train_data = pd.read_csv("UCI+HAR+Dataset/train/X_train.txt", header=None, sep='\s+')
-train_label = pd.read_csv("UCI+HAR+Dataset/train/Y_train.txt", header=None, sep='\s+')
+    merged_dataframe = train_data.merge(train_label, how='outer', left_index=True, right_index=True)
 
-merged_dataframe = train_data.merge(train_label, how='outer', left_index=True, right_index=True)
+    print(" --- data --- ")
+    print(train_data.info())
+    print(" --- label --- ")
+    print(train_label.info())
+    print(" --- merged --- ")
+    print(merged_dataframe.info())
 
-print(" --- data --- ")
-print(train_data.info())
-print(" --- label --- ")
-print(train_label.info())
-print(" --- merged --- ")
-print(merged_dataframe.info())
+    dataframes = [merged_dataframe[merged_dataframe['0_y'] == i] for i in range(1, 7)]
 
-dataframes = [merged_dataframe[merged_dataframe['0_y'] == i] for i in range(1, 7)]
+    for k in range(1, 11):
+        plt.figure()
+        for dataframe in dataframes:
+            sns.kdeplot(dataframe.loc[:, k], legend=False)
+        plt.legend(["{}".format(ACTIVITY_DICT[i]) for i in range(1, 7)])
+        plt.title("Plot for feature {}".format(k))
+    plt.show()
 
-for k in range(1, 11):
-    plt.figure()
-    for dataframe in dataframes:
-        sns.kdeplot(dataframe.loc[:, k], legend=False)
-    plt.legend(["{}".format(ACTIVITY[i]) for i in range(1, 7)])
-    plt.title("Plot for feature {}".format(k))
-plt.show()
-
-print("The distributions plotted are Gaussians; either a single or a sum of a couple.")
-print("Distinction on label is quite hard to do base on a single or couple of plots.")
-print("For example feature 1 has different heights, but similar mean and spread for each of the activities")
-print("This is a poor measure, since variaties in height occur naturally.")
-print("Looking at the plots of feature 4 and feature 5, we are hover able to detect the")
-print("difference between passive and active activities. The peaks are clearly separated.")
+    print("The distributions plotted are Gaussians; either a single or a sum of a couple.")
+    print("Distinction on label is quite hard to do base on a single or couple of plots.")
+    print("For example feature 1 has different heights, but similar mean and spread for each of the activities")
+    print("This is a poor measure, since variaties in height occur naturally.")
+    print("Looking at the plots of feature 4 and feature 5, we are hover able to detect the")
+    print("difference between passive and active activities. The peaks are clearly separated.")
--- Data_science_--_TS/Exercise_5.py
+++ Data_science_--_TS/Exercise_5.py
@@ -1,3 +1,4 @@
+from Exercise_3 import ACTIVITY_DICT
 from Exercise_4 import merged
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
@@ -6,17 +7,16 @@ import math
 
 activities = [merged[merged['label'] == i] for i in range(1, 7)]
 
-characteristics = np.ndarray([])
+characteristics = []
 
 for activity in activities:
     characteristic = {
-        "label": activity["label"].values[0],
+        "label": ACTIVITY_DICT[activity["label"].values[0]],
         "mean": activity["val"].mean(),
         "variance": activity["val"].var(),
         "skewness": activity["val"].skew(),
         "kurtosis": activity["val"].kurtosis(),
         "spread": activity["val"].max()-activity["val"].min(),
-
     }
     characteristics = np.append(characteristics, characteristic)
 
@@ -26,8 +26,10 @@ if __name__ == '__main__':
     for characteristic in characteristics:
         plt.plot(x, mlab.normpdf(x, characteristic['mean'], math.sqrt(characteristic['variance'])))
     plt.title("Gaussian plot of activities")
-    plt.legend(["activity {}".format(characteristic['label']) for characteristic in characteristics])
+    plt.legend(["{}".format(characteristic['label']) for characteristic in characteristics])
     plt.show()
 
+    print("Based on the plot for the charactestics we selected, it is clear that one can discern")
+    print("between active and passive activities.")
 
 
